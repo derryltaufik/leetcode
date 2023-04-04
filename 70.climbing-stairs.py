a1f4835106a9fc729a366ba54722923eb4ff600d
Derryl Taufik <derryldeveloper@gmail.com>
2023-04-04 23:25:09 +0700

    70 added alternative @cache functools

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -8,20 +8,29 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        memo = {}
+        # memo = {}
 
-        def recur(steps):
+        # def recur(steps):
+        #     if steps <= 1:
+        #         return 1
+
+        #     if steps in memo:
+        #         return memo[steps]
+
+        #     res = recur(steps - 1) + recur(steps - 2)
+        #     memo[steps] = res
+        #     return res
+
+        # return recur(n)
+
+        # using @cache
+        @lru_cache
+        def recur2(steps):
             if steps <= 1:
                 return 1
+            return recur2(steps - 1) + recur2(steps - 2)
 
-            if steps in memo:
-                return memo[steps]
-
-            res = recur(steps - 1) + recur(steps - 2)
-            memo[steps] = res
-            return res
-
-        return recur(n)
+        return recur2(n)
 
 
 # @lc code=end
