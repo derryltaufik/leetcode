a3eb33ef5abf98a53e69bb36e9e457665810c963
Derryl Taufik <derryldeveloper@gmail.com>
2023-04-04 23:23:15 +0700

    70 increased readibility

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -6,22 +6,22 @@
 
 # @lc code=start
 class Solution:
-    memo = []
-
-    def recur(self,steps):
-        if(steps <=1):
-            return 1
-        
-        if self.memo[steps] != None:
-            return self.memo[steps]
-        else:
-            self.memo[steps] = self.recur(steps-1) + self.recur(steps-2)
-            return self.memo[steps]
-        
-
     def climbStairs(self, n: int) -> int:
-        self.memo = [None] * (n+1)
-        return self.recur(n)
+
+        memo = {}
+
+        def recur(steps):
+            if steps <= 1:
+                return 1
+
+            if steps in memo:
+                return memo[steps]
+
+            res = recur(steps - 1) + recur(steps - 2)
+            memo[steps] = res
+            return res
+
+        return recur(n)
+
 
 # @lc code=end
-
