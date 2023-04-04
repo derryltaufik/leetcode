913866007019fefde17e7720cf95f10af795402c
Derryl Taufik <derryldeveloper@gmail.com>
2023-04-04 22:28:20 +0700

    235

--- 235.lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235.lowest-common-ancestor-of-a-binary-search-tree.py
@@ -17,15 +17,38 @@ class Solution:
     def lowestCommonAncestor(
         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
     ) -> "TreeNode":
-        a = min(p.val, q.val)
-        b = max(p.val, q.val)
-        while root:
-            if root.val < a:
-                root = root.right
-            elif root.val > b:
-                root = root.left
-            else:
-                return root
+        # a = min(p.val, q.val)
+        # b = max(p.val, q.val)
+        # while root:
+        #     if root.val < a:
+        #         root = root.right
+        #     elif root.val > b:
+        #         root = root.left
+        #     else:
+        #         return root
+
+        # using recursion
+
+        small = big = 0
+        if p.val > q.val:
+            small = q.val
+            big = p.val
+        else:
+            small = p.val
+            big = q.val
+
+        def search(node, small, big):
+            if node is None:
+                return None
+
+            if small <= node.val <= big:
+                return node
+            elif node.val > small:
+                return search(node.left, small, big)
+            elif node.val < big:
+                return search(node.right, small, big)
+
+        return search(root, small, big)
 
 
 # @lc code=end
