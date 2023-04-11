2c6093db49b5a16b710ece4242606da306ff3cce
Derryl Taufik <derryldeveloper@gmail.com>
2023-04-11 22:37:07 +0700

    1672

---
+++ 1672.richest-customer-wealth.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=1672 lang=python3
+#
+# [1672] Richest Customer Wealth
+#
+
+# @lc code=start
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        reduced = [sum(i) for i in accounts]
+        return max(reduced)
+
+
+# @lc code=end
